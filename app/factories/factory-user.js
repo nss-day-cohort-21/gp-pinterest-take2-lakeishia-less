9a0e8fade7c98ed6aa403fb597bec55646063d7e
AustinJ <austin.jay.kurtis@gmail.com>
2017-09-05 14:49:40 -0500

    factory User

---
+++ app/factories/factory-user.js
@@ -0,0 +1,37 @@
+"use strict";
+
+app.factory('PinApp', function($q, $http){
+
+	let currentUser = null;
+
+	const userLoggedIn = function(){
+		console.log('PinApp: userLoggedIn');
+		return new Promise((resolve, reject) =>{
+			firebase.auth().onAuthStateChanged((user) =>{
+				if(user){
+					currentUser = user.uid;
+					console.log("user", user.uid);
+					resolve(true)
+				}else{
+					resolve(false);
+				}
+			});
+		});
+	};
+
+	const getCurrentUser = function(){
+		return currentUser;
+	};
+
+	const logOut = function(){
+		console.log("logOutUser");
+		return.firebase.auth().signOut();
+	};
+
+	let googleAuth = new firebase.auth.GoogleAuthProvider();
+
+	let authWithGoogle = function(){
+		return firebase.auth().signInWithPopup(googleAuth);
+	};
+	return {userLoggedIn, getCurrentUser, logOut, authWithGoogle};
+});
