59f494813c6a63dc413e37b7809a96fa396c4c7f
Matt Minner <matthewfminner@gmail.com>
2017-09-06 15:01:39 -0500

    Delete fb-creds.js

--- app/values/fb-creds.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-app.constant("FBCreds", {
-    apiKey: "AIzaSyDQc4QDf35jRV5eX5SmAsQN7Y3alfn6bPA",
-    authDomain: "music-history-codealong.firebaseapp.com",
-    databaseURL: "https://music-history-codealong.firebaseio.com"
-});
